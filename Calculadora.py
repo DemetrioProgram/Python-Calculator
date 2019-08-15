ece4e9bce3eb14ca7ed1ef984e6daeca04d54bb3
DemetrioProgram <43687376+DemetrioProgram@users.noreply.github.com>
2019-08-15 17:15:29 -0300

    Espero que gostem!!!

---
+++ Calculadora.py
@@ -0,0 +1,11 @@
+num = input('Digite um valor:')
+num1 = input('Digite outro valor:')
+soma = int(num) + int(num1)
+sub = int(num) - int(num1)
+div = int(num) / int(num1)
+mul = int(num) * int(num1)
+
+print('A soma entre', num, 'e', num1, 'é', soma)
+print('A divisão entre', num, 'e', num1, 'é', sub)
+print('A divisão entre', num, 'e', num1, 'é', div)
+print('A multiplicação entre', num, 'e', num1, 'é', mul)
